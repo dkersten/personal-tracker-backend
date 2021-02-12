7ea5458edd61eca15ef627cb9b7449be303970fa
dkersten <dkersten1994@gmail.com>
2021-02-11 22:16:35 -0500

    added rest of CRUD functinality

--- src/api/controllers/activityController.js
+++ src/api/controllers/activityController.js
@@ -8,9 +8,39 @@ exports.all_activities = (req, res) => {
     });
 };
 
-exports.activity = (req, res) => {
+exports.create_an_activity = (req, res) => {
+    const newActivity = new activity(req.body);
+    newActivity.save((err, activity) => {
+      if (err) res.send(err);
+      res.json(activity);
+    });
+  };
+
+exports.read_activity = (req, res) => {
     activity.findById(req.params.activityId, (err, activity) => {
         if (err) res.send(err);
         res.json(activity)
     })
 }
+
+exports.update_an_activity = (req, res) => {
+    activity.findOneAndUpdate(
+      { _id: req.params.activityId },
+      req.body,
+      { new: true },
+      (err, activity) => {
+        if (err) res.send(err);
+        res.json(activity);
+      }
+    );
+  };
+
+  exports.delete_an_activity = (req, res) => {
+    activity.deleteOne({ _id: req.params.activityId }, err => {
+      if (err) res.send(err);
+      res.json({
+        message: 'activity successfully deleted',
+       _id: req.params.activityId
+      });
+    });
+  };
